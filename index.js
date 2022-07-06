a1e35feb5c3fa6039a8c219ccaf850e7b87b86aa
IqdamRA <rasifalfarobi@gmail.com>
2022-07-06 03:37:27 -0700

    backend

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require ('express');
+const { MongoClient } = require('mongodb');
+const app = express ();
+const port = 5000;
+
+const url = 'mongodb+srv://uas:backend@mongodb.7cgztlg.mongodb.net/test';
+const client = new MongoClient(url);
+const dbName = 'uas';
+
+
+app.get("/",async (req,res)=> {
+    // Use connect method to connect to the server
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db(dbName);
+    const collection = db.collection('backend');
+    const database = await collection.find({}).toArray();
+
+    res.status(200).json({database});
+  });
+
+app.listen(port,()=> {
+    console.log(`Example app listening on port ${port}`);
+});
